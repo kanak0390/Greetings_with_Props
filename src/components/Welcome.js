01f07bf74d23431f38604e06080f6fa6994204a2
kanakroy0390 <kanak0390@gmail.com>
2023-02-11 12:07:05 +0530

    C:\Users\Lalan\React\Greetings With Props

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Welcome(props){
+    return(
+        <>
+          <h1>Hey {props.name}!</h1>
+          <h2>Welcome to Newton School.</h2>
+        </>
+    )
+}
+
+export default Welcome;
